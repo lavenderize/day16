31165788706970952376966beb4931cbc184fdb1
lavenderize <liexquisit@gmail.com>
2023-02-03 11:17:49 +0900

    linear list 02

--- day15.py
+++ day15.py
@@ -1,16 +1,25 @@
-pokemons = list()
+pokemons = ["피카츄", "라이츄", "파이리", "꼬부기", "이상해씨"]
+
 
 def add_data(pokemon) :
     pokemons.append(None)
 
-    pokemons[-1] = pokemon
 
-add_data('피카츄')
-add_data('라이츄')
-add_data('파이리')
-add_data('꼬부기')
-add_data('이상해씨')
+def insert_data(idx, pokemon):
+    if idx < 0 or idx > len(pokemon):
+        print("Out of range!")
+        return
 
+    pokemons.append(None)
+
+    for i in range(len(pokemons) - 1, idx, -1):
+        pokemons[i] = pokemons[i - 1]
+        pokemons[i - 1] = None
+
+    pokemons[idx] = pokemon
+
+insert_data(1, "치코리타")
+insert_data(3, "잠만보")
 print(pokemons)
 
 
