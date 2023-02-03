a088f14f6f42e00dbbe07defca312ac354b70f2f
lavenderize <liexquisit@gmail.com>
2023-02-03 11:08:24 +0900

    linear list 01

---
+++ day15.py
@@ -0,0 +1,17 @@
+pokemons = list()
+
+def add_data(pokemon) :
+    pokemons.append(None)
+
+    pokemons[-1] = pokemon
+
+add_data('피카츄')
+add_data('라이츄')
+add_data('파이리')
+add_data('꼬부기')
+add_data('이상해씨')
+
+print(pokemons)
+
+
+
