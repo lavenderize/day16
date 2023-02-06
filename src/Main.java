e9ebe3c1e6d0e99231f3b82e92744a03e92ee0b0
lavenderize <liexquisit@gmail.com>
2023-02-06 09:40:06 +0900

    node 01

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Node node = new Node();
+        node.data = "Pikachu";
+        System.out.println(node.toString());
+    }
+}
